a50a4146d914b9c07fd6804af47f7c4a34bf63de
DevMajed <majed.xc@gmail.com>
2024-01-25 02:26:46 -0500

    Code is Complete

--- src/main.c
+++ src/main.c
@@ -1,3 +1,25 @@
+/*
+    Project: ESP-IDF Based 8X8 Matrix LED with Shift Register
+    URL: https://github.com/DevMajed/ESP-IDF-Based-8X8-Matrix-with-Shift-Register.git
+    Author: Majed Alharbi [ Majed Dev ]
+
+    DISCLAIMER
+    1. While I have thoroughly tested and approved my code, please use it at your own risk.
+
+    2. You might come across some recurring comments in my code. I include them to ensure that readers
+    can understand how everything works, even if they haven't seen my other projects. I apologize for any redundancy.
+
+    3. The purpose of these codes is to facilitate an understanding of how the ESP-IDF framework works,
+    especially since I struggled to learn it due to the lack of available resources.
+
+    4. I consistently adopt a Code Modular Design approach for all my projects. This enhances readability, maintainability,
+    and reusability. Be sure to explore other source files and headers to gain a complete understanding of the project.
+
+    LICENSE
+    Users are free to utilize, modify, or reproduce the code and its derivatives.
+*/
+
+
 // Header of Led_Matrix.C
 #include "led_matrix.h"
 
@@ -14,9 +36,9 @@ static const char *TAG = "MAIN";
 #include <freertos/task.h>
 #include "driver/gpio.h"
 
-// Array holding the data ( numbers and letters ) 0-9 and A-F
-const uint8_t data[] ={
 
+// Array holding the data ( numbers and letters ) 0-9 and A-F
+const uint8_t data [] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // " "
     0x00, 0x00, 0x01, 0x7F, 0x21, 0x00, 0x00, 0x00, // "1"
     0x00, 0x00, 0x31, 0x49, 0x45, 0x23, 0x00, 0x00, // "2"
@@ -34,14 +56,12 @@ const uint8_t data[] ={
     0x00, 0x00, 0x3E, 0x41, 0x41, 0x7F, 0x00, 0x00, // "D"
     0x00, 0x00, 0x41, 0x49, 0x49, 0x7F, 0x00, 0x00, // "E"
     0x00, 0x00, 0x40, 0x48, 0x48, 0x7F, 0x00, 0x00 // "F"
-
 };
 
+
 // Array holding the data for smiling face
-const uint8_t smillingFace[] ={
-
+const uint8_t smillingFace [] = {
     0x1C, 0x22, 0x51, 0x45, 0x45, 0x51, 0x22, 0x14
-
 };
 
 
@@ -60,13 +80,51 @@ static void init_hw(void) {
         .pull_down_en = 0,
     };
 
-    // Calling config function by passing the address of the io_conf struct
-    // This is to make the settings for the GPIO.
+    // Calling config function by passing the address of the io_conf struct to set GPIO
     gpio_config(&io_conf);
 }
 
-void app_main() {
 
+void loop () {
+// Display Smiling Face
+for (int i = 0; i < 50; i++) {
+    // Initilizing Column data to first Bit to be synchronized with row operation
+    int cols = 0x01;
+    // Printing smiling face row data
+    for (int j = 0; j  < 8; j++){
+        // calling row function for each row of smiling face
+        matrixRowsVal (smillingFace[j]);
+        // scanning columns one by one ( ~ because of common anode LED configtraion)
+        matrixColsVal (~cols);
+        // Little dellay before the next row / column
+        vTaskDelay (10 / portTICK_PERIOD_MS);
+        // Shifting cols to move to the next column bit
+        cols = cols << 1;
+    }
+}
+
+
+// Display numbres-letters 0-9 and A-F, 128 bytes of data.
+for (int i = 0; i < 128; i++) {
+    // Initilizing Column data to first Bit to be synchronized with row operation
+    int cols = 0x01;
+    // Display 8 bytes of data ( a number or a letter ) then the next ...etc
+    for ( int j = i; j < 8 + i; j ++) {
+        // row data of the number or letter
+        matrixRowsVal (data[j]);
+        // scanning columns one by one ( ~ because of common anode LED configtraion)
+        matrixColsVal (~cols);
+        // Little dellay before the next row / column
+        vTaskDelay (10/portTICK_PERIOD_MS);
+         // Shifting cols to move to the next column bit
+        cols = cols << 1;
+    }
+}
+// end of loop function
+}
+
+
+void app_main() {
     // Set the log level for the TAG to ESP_LOG_DEBUG
     esp_log_level_set(TAG, ESP_LOG_DEBUG);
     ESP_LOGE(TAG, "This is an error");
@@ -75,11 +133,12 @@ void app_main() {
     ESP_LOGD(TAG, "This is a debug");
     ESP_LOGV(TAG, "This is a verbose");
 
-    
+    // initlize hardware and setup GPIO
+    init_hw();
 
-
-
-
-
-
+    // interate forever through the loop function
+    while (1) {
+        // calling the loop function
+        loop();
+    }
 }
